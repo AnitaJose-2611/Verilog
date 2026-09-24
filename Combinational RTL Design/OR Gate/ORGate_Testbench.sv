20443f8733d249b9eed63e4785fe6bc8fefc1dc8
Anita Jose <joseanita2611@gmail.com>
2026-09-24 18:47:48 +0530

    Testbench Code for OR Gate

---
+++ Combinational RTL Design/OR Gate/ORGate_Testbench.sv
@@ -0,0 +1,19 @@
+module tb;
+  reg a;
+  reg b;
+  wire out;
+  ORGate uut1(.a(a) , .b(b) , .out(out));
+  initial begin
+    a = 0 ; b = 0;
+    #10;
+    a = 0 ; b = 1;
+    #10;
+    a = 1 ; b = 0;
+    #10;
+    a = 1 ; b = 1;
+    #10;
+  end
+  initial begin
+    $monitor("a = %0d \t b = %0d \t out = %0d" , a , b , out);
+  end
+endmodule
